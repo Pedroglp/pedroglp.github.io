a72c3b3b96bbf387c0683a8bfb2afb1ce954a760
pedroglp <pedrogabriellancelloti@poli.ufrj.br>
2018-05-17 18:18:20 -0300

    adicionando texto, fixando botao

--- src/assets/translations.js
+++ src/assets/translations.js
@@ -6,8 +6,16 @@ export default {
         avatar: {role:'Full Stack Developer'}
     },
     pt:{
-        about: {title: 'Sobre Mim', text:''},
-        experiences: {title: 'Experiências', text:''},
+        about: {title: 'Sobre Mim', text:'<p>Recém formado pela Universidade Federal do Rio de Janeiro,apaixonado por novas tecnologias e pensar em suas aplicações para o mercado.  Durante os projetos,  busca desenvolver utilizando as boas práticas de programação como DRY e TDD. Sempre busca participar dos processos criativos,  visando aprendizado e crescimento pessoal.</p>'},
+        experiences: {title: 'Experiências', text:`<h2>T.T Burger Alimentos (2016-2018)</h2>
+                                                    <p>Durante a minha trajetória na empresa sempre tive muita liberdade, o que me rendeu
+                                                    experiências muito completas que incluiam o processo de desenho da solução até sua implementação final.</p>
+                                                    <p>Nesses dois anos em que percorri de estagiário até minha efetivação, posso citar alguns projetos como:</p>
+                                                    <p><b>EncanTT</b> - Aplicativo Android, com Backend em utilizando Django Rest Framework e Banco de Dados Postgresql.</p>
+                                                    <p><b>QRobbie</b> - Gerador de Códigos QR para autenticação de ofertas integrado com o Banco de Dados do EncanTT.</p>
+                                                    <p><b>ChatBoTT</b> - Chatbot para realização de pedidos via Facebook utilizando ClaudiaJS, DynamoDB com arquitetura em Microserviços.</p>
+                                                    <p><b>Portal Interno</b> - Portal Web (Django) para administração da Empresa Integrado com as soluções acima.</p>
+                                                    <p><b>Metabase</b> - Para auxiliar nas decisões estratégicas, sugeri a utilização do Metabase como ferrramenta de B.I. e integrei com nosso Banco de Dados.</p>`},
         skills: {title: 'Habilidades', text:''},
         avatar: {role:'Desenvolvedor Full Stack'}
     }
